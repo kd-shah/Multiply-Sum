0aecb1ee015fe3fdc12c2bcea22aba5841842db0
Kunal Shah <kunal.190410107121@gmail.com>
2023-07-10 17:08:52 +0530

    initial commit

---
+++ Sum/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Sum
+{
+    internal class Program
+    {
+        static int Sum(int a, int b) {
+            int c;
+            c = a - b;
+            return c;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter second number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Sum of two number is : {0}", Sum(num1, num2));
+
+        }
+       
+    }
+}
